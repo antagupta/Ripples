016468d99235d7f1dda06ba7f143500524e3b326
antagupta <antagupta@deloitte.com>
2024-09-04 19:44:35 +0530

    Initial commit

---
+++ Ripple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ripple
+//
+//  Created by Gupta, Antara on 04/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by Gupta, Antara on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
